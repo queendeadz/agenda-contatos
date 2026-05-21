b8c9d6ed31a6e38bfeb5884311a2af19f8062841
queendeadz <eppluged@gmail.com>
2026-05-21 16:29:44 -0300

    comite 3

--- agenda.py
+++ agenda.py
@@ -3,21 +3,39 @@ AGENDA = {}
 AGENDA ["sarah"] = {
     "telefone": "1195763122",
     "email": "sarah@gmail.com",
-    "endereco": "avenida das avenidas n 999",
+    "endereco": "avenida n1",
 }
 AGENDA ["maria"] = {
     "telefone": "1195333322",
     "email": "maria@gmail.com",
-    "endereco": "avenida n 999",
+    "endereco": "avenida n2",
 }
+
 
 def mostrar_contatos():
     for contato in AGENDA:
-        print ("Nome:", contato)
-        print("Telefone:", AGENDA[contato] ["telefone"])
-        print("Email:", AGENDA[contato] ["email"])
-        print("Endereco:", AGENDA[contato] ["endereco"])
+        buscar_contato(contato)
+        print ("=================")
 
-        print ("===========")
 
+def buscar_contato(contato):
+    print('Nome: ', contato)
+    print("Telefone:", AGENDA[contato]["telefone"])
+    print("Email:", AGENDA[contato]["email"])
+    print("Endereco:", AGENDA[contato]["endereco"])
+
+
+def incluir_contato(contato, telefone, email, endereco):
+    AGENDA[contato] = {
+    "telefone": telefone,
+    "email": email,
+    "endereco": endereco,
+    }
+    print('====== Contato {} adicionado com sucesso ======'.format(contato))
+
+#buscar_contato("abel")
+incluir_contato('abel', '1138462384632', 'abel@gmail.com', 'avenida n3')
 mostrar_contatos()
+
+
+
